44d065c5c164245ab3c6ccafaab86657af5446d1
iccee0 <git@roeh.ch>
2022-12-05 07:02:30 +0000

    new file:   4/run.ts

---
+++ 4/run.ts
@@ -0,0 +1,12 @@
+import elfgroups from "./input.json" assert { type: "json" };
+
+let runner = 0;
+elfgroups.map(elfs=>{
+    runner += elfs.elf1.start >= elfs.elf2.start && elfs.elf1.end <= elfs.elf2.end || elfs.elf2.start >= elfs.elf1.start && elfs.elf2.end <= elfs.elf1.end ? 1 : 0
+})
+console.log(runner);
+runner = 0;
+elfgroups.map(elfs=>{
+    runner += elfs.elf1.start >= elfs.elf2.start && elfs.elf1.end >= elfs.elf2.start || elfs.elf1.start >= elfs.elf2.end && elfs.elf1.end >= elfs.elf2.end || elfs.elf2.start >= elfs.elf1.start && elfs.elf2.end >= elfs.elf1.start || elfs.elf2.start >= elfs.elf1.end && elfs.elf2.end >= elfs.elf1.end ? 1 : 0
+})
+console.log(runner);
